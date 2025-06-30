aaa15bf9d95bc369036db702bbca282e7fc43a34
Alfons Caroles <cllin7787@gmail.com>
2025-06-30 12:07:35 +1000

    Initial setup Next with Chakra UI

---
+++ src/components/ui/provider.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
+import { ThemeProvider } from "next-themes";
+import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
+
+export function Provider(props: ColorModeProviderProps) {
+  return (
+    <ThemeProvider>
+      <ChakraProvider value={defaultSystem}>
+        <ColorModeProvider {...props} />
+      </ChakraProvider>
+    </ThemeProvider>
+  );
+}
