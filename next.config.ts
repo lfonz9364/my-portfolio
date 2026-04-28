cd52a35b215d51080ad7f47679af5c85c92c5196
Alfons Caroles <cllin7787@gmail.com>
2026-04-28 14:51:28 +1000

    Resolving issues with alias import due to next 16 turbopack default

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   experimental: {
     optimizePackageImports: ["@chakra-ui/react"],
   },
+  turbopack: {
+    resolveAlias: {
+      "@/*": "./src/*",
+    },
+  },
   images: {
     remotePatterns: [
       {
