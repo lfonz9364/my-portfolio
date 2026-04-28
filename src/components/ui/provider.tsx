ce9155e987b33d37618650516951441e53297978
Alfons Caroles <cllin7787@gmail.com>
2026-04-28 15:01:58 +1000

    Resolving next themes issue in next 16.2.1

--- src/components/ui/provider.tsx
+++ src/components/ui/provider.tsx
@@ -1,7 +1,8 @@
 "use client";
 
 import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
-import { ThemeProvider } from "next-themes";
+// https://github.com/pacocoursey/next-themes/issues/387 to resolve issue with next themes
+import { ThemeProvider } from "@teispace/next-themes";
 import { ColorModeProvider, type ColorModeProviderProps } from "./color-mode";
 
 export function Provider(props: ColorModeProviderProps) {
