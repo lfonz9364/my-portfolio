4bae4bafc3456207fc0ba64bbc0505808d8cd768
Alfons Caroles <cllin7787@gmail.com>
2026-04-28 16:26:57 +1000

    Updating queries to have types

--- src/lib/contenful-queries.ts
+++ src/lib/contenful-queries.ts
@@ -1,10 +1,18 @@
 import { contentfulClient, contentfulPreviewClient } from "./contentful";
+import {
+  ExperienceEntry,
+  ExperienceSkeleton,
+  HeroEntry,
+  HeroSkeleton,
+  ProjectEntry,
+  ProjectSkeleton,
+} from "./definitions";
 
 const getClient = (preview = false) =>
   preview ? contentfulPreviewClient : contentfulClient;
 
-export const getProjects = async (preview = false) => {
-  const entries = await getClient(preview).getEntries({
+export const getProjects = async (preview = false): Promise<ProjectEntry[]> => {
+  const entries = await getClient(preview).getEntries<ProjectSkeleton>({
     content_type: "project",
     order: ["-fields.featured", "-sys.createdAt"],
   });
@@ -12,18 +20,23 @@ export const getProjects = async (preview = false) => {
   return entries.items;
 };
 
-export const getProjectBySlug = async (slug: string, preview = false) => {
-  const entries = await getClient(preview).getEntries({
+export const getProjectBySlug = async (
+  slug: string,
+  preview = false,
+): Promise<ProjectEntry | null> => {
+  const entries = await getClient(preview).getEntries<ProjectSkeleton>({
     content_type: "project",
     "fields.slug": slug,
     limit: 1,
   });
 
-  return entries.items[0] || null;
+  return entries.items[0] ?? null;
 };
 
-export const getExperiences = async (preview = false) => {
-  const entries = await getClient(preview).getEntries({
+export const getExperiences = async (
+  preview = false,
+): Promise<ExperienceEntry[]> => {
+  const entries = await getClient(preview).getEntries<ExperienceSkeleton>({
     content_type: "experience",
     order: ["-fields.startDate"],
   });
@@ -31,11 +44,13 @@ export const getExperiences = async (preview = false) => {
   return entries.items;
 };
 
-export const getHeroSection = async (preview = false) => {
-  const entries = await getClient(preview).getEntries({
+export const getHeroSection = async (
+  preview = false,
+): Promise<HeroEntry | null> => {
+  const entries = await getClient(preview).getEntries<HeroSkeleton>({
     content_type: "heroSection",
     limit: 1,
   });
 
-  return entries.items[0] || null;
+  return entries.items[0] ?? null;
 };
