d6f47d9f2c8371ecd499fe5f9655c8db5070d604
Alfons Caroles <cllin7787@gmail.com>
2026-06-04 21:22:01 +1000

    Replaced Navbar into Header

---
+++ src/components/organisms/Header.tsx
@@ -0,0 +1,43 @@
+import { Box, Container, Flex, Heading } from "@chakra-ui/react";
+import Icon from "@public/logo.svg";
+import Image from "next/image";
+
+export const Header = () => (
+  <Box
+    as="header"
+    position="sticky"
+    top={0}
+    zIndex={50}
+    bg="whiteAlpha.900"
+    borderBottomWidth="1px"
+    borderColor="gray.200"
+    backdropFilter="blur(10px)"
+  >
+    <Container maxW="6xl" paddingInline="4">
+      <Flex h="32" align="center" justify="space-between">
+        <Box
+          pos="relative"
+          w={{ base: 16, sm: 24, md: 32 }}
+          h={{ base: 16, sm: 24, md: 32 }}
+        >
+          <Image
+            src={Icon.src}
+            alt="Fonzie Logo"
+            fill
+            sizes="(max-width: 768px) 40vw, 20vw"
+            className="object-fit"
+          />
+        </Box>
+
+        <Heading
+          color="#62D5F5"
+          as="h1"
+          size={{ base: "2xl", sm: "3xl", md: "5xl" }}
+          lineHeight="1.05"
+        >
+          Alfons Caroles - Fonzie
+        </Heading>
+      </Flex>
+    </Container>
+  </Box>
+);
--- src/components/templates/PageLayout.tsx
+++ src/components/templates/PageLayout.tsx
@@ -1,11 +1,11 @@
 import { Container } from "@/components/atoms/Container";
 import { Footer } from "@/components/organisms/Footer";
-import { NavBar } from "@/components/organisms/NavBar";
+import { Header } from "@/components/organisms/Header";
 import { Box } from "@chakra-ui/react";
 
 export const PageLayout = ({ children }: { children: React.ReactNode }) => (
   <Box bg="gray.50" color="gray.900" minH="100vh">
-    <NavBar />
+    <Header />
 
     <Container py={{ base: 8, md: 12 }}>{children}</Container>
 
