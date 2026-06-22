15119f79539bb1898fd336190012f3054b195853
Alfons Caroles <cllin7787@gmail.com>
2026-06-22 18:39:02 +1000

    Removed unused pages and templates

--- src/components/templates/ProjectsTemplate.tsx
+++
@@ -1,6 +0,0 @@
-import { Project } from "@/types/contenful";
-import { ProjectsGrid } from "@/components/organisms/ProjectsGrid";
-
-export const ProjectsTemplate = ({ projects }: { projects: Project[] }) => (
-  <ProjectsGrid projects={projects} />
-);
