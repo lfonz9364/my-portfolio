6dcdccbbd92b78eb1624d98e00927fa376969e5e
Alfons Caroles <cllin7787@gmail.com>
2026-06-22 15:59:30 +1000

    Updated ExperienceTimeline component

--- src/components/organisms/ExperienceTimeline.tsx
+++ src/components/organisms/ExperienceTimeline.tsx
@@ -1,22 +1,14 @@
 import { ExperienceCard } from "@/components/molecules/ExperienceCard";
-import { Section } from "@/components/molecules/Section";
-import { Experience } from "@/types/contenful";
-import { SimpleGrid } from "@chakra-ui/react";
+import { ExperienceTimelineProps } from "@/types/componentsCustomProps";
+import { CardsGroup } from "../molecules/CardsGroup";
 
 export const ExperienceTimeline = ({
+  content,
   experiences,
-}: {
-  experiences: Experience[];
-}) => (
-  <Section
-    name="experiences"
-    title="Work Experience"
-    desc="A snapshot of my frontend, full-stack, and mobile development experience."
-  >
-    <SimpleGrid columns={{ base: 1, md: 2, lg: 3 }} gap={6}>
-      {experiences.map((experience) => (
-        <ExperienceCard key={experience.sys.id} experience={experience} />
-      ))}
-    </SimpleGrid>
-  </Section>
+}: ExperienceTimelineProps) => (
+  <CardsGroup headerContent={content}>
+    {experiences.map((experience) => (
+      <ExperienceCard key={experience.sys.id} experience={experience} />
+    ))}
+  </CardsGroup>
 );
